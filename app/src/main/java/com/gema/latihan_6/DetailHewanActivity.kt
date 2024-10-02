da2a9747a07248d02e50eba6e1ee68e3e4865683
gema <gemasyaputra04@gmail.com>
2024-10-02 20:55:26 +0700

    update 2

---
+++ app/src/main/java/com/gema/latihan_6/DetailHewanActivity.kt
@@ -0,0 +1,25 @@
+package com.gema.latihan_6
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class DetailHewanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_detail_hewan)
+
+        val des = intent.getStringExtra("desk")
+        val deskripsi = findViewById<TextView>(R.id.des)
+        deskripsi.setText(des)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
--- app/src/main/java/com/gema/latihan_6/list.kt
+++ app/src/main/java/com/gema/latihan_6/list.kt
@@ -1,5 +1,6 @@
 package com.gema.latihan_6
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.ListView
@@ -21,9 +22,15 @@ class list : AppCompatActivity() {
         lv_item = findViewById(R.id.n_hewan)
 
 
-        val nama_hewan = listOf("gajah","beruang","ikan","lumba-lumba","ular","bekicot","sapi","kambing")
+        val nama_hewan = arrayOf("gajah","beruang","ikan","lumba-lumba","harimau")
 
         //kita masukan data array hewan ke adapter
+
+        val hewan_desk = arrayOf("gajah adalah hewan yang lucu dan besar.",
+            "beruang adalah hewan yang ganas dan pelindung anaknya.",
+            "ikan adalah hewan air yang suka berenang.",
+            "lumba-lumba adalah hewan yang pintar dan cerdas.",
+            "Harimau adalah kucing besar yang buas dan kuat.")
 
         lv_item.adapter = ArrayAdapter(this ,android.R.layout.simple_list_item_1,nama_hewan)
         // listview widget --> adapter --> data source (array list)
@@ -34,6 +41,10 @@ class list : AppCompatActivity() {
             parent,view,position , id ->
             Toast.makeText(this,"anda memilih : ${nama_hewan[position]}",
                 Toast.LENGTH_SHORT).show()
+        val intent = Intent(this@list,DetailHewanActivity::class.java)
+
+            intent.putExtra("desk",hewan_desk[position])
+            startActivity(intent)
         }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
